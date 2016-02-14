f02fac9b0daeefd215542493d785ba6b277dc5b2
Mopsalarm <mopsalarm@users.noreply.github.com>
2016-02-14 21:38:31 +0100

    Use glide to handle dependencies

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+set -e
 
-docker run --rm \
-  -v "$(pwd):/src" \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  centurylink/golang-builder \
-  mopsalarm/pr0gramm-meta-rest
+#docker run --rm \
+#  -v "$(pwd):/src" \
+#  -v /var/run/docker.sock:/var/run/docker.sock \
+#  centurylink/golang-builder \
+#  mopsalarm/pr0gramm-meta-rest
 
+go build
+docker build -t mopsalarm/pr0gramm-meta-rest .
 docker push mopsalarm/pr0gramm-meta-rest
